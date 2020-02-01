b8851e1a92ad1dc4aceba5404099b4e1df169170
ienokado <ienokado.kk@gmail.com>
2020-02-01 19:19:10 +0900

    検索ヒットしなかった場合の画面レイアウト調整

--- src/resources/views/common/footer.blade.php
+++ src/resources/views/common/footer.blade.php
@@ -1,22 +1,32 @@
-<footer class="footer">
+<!-- Footer -->
+<footer class="page-footer font-small fixed-bottom">
     <div class="hr"></div>
-
-    <div class="menu-items">
-        <a href="#" title="プライバシーポリシー">プライバシーポリシー</a>
-        <a href="#" title="利用規約">利用規約</a>
-        <a href="#" title="お問い合わせ" href="mailto:info@pointy.love">お問い合わせ</a>
-        <div></div>
-        <a href="{{ Config::get('const.sns_share_url.facebook') }}"><i class="fab fa-facebook-square"></i></a>
-        <a href="{{ Config::get('const.sns_share_url.twitter') }}"><i class="fab fa-twitter"></i></a>
-        <a href="{{ Config::get('const.sns_share_url.line') }}"><i class="fab fa-line"></i></a>
-    </div>
-
+    <!-- Footer Elements -->
     <div class="container">
-        <div class="row text-center">
-            <div class="copyright">
-                <p>Copyright © 2020 All rights reserved</p>
+        <!-- Grid row-->
+        <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-12 py-5">
+                <div class="mb-5 flex-center menu-items">
+                    <!-- Facebook -->
+                    <a href="{{ Config::get('const.sns_share_url.facebook') }}"><i class="fab fa-facebook-square"></i></a>
+                    <!-- Twitter -->
+                    <a href="{{ Config::get('const.sns_share_url.twitter') }}"><i class="fab fa-twitter"></i></a>
+                    <!-- LINE -->
+                    <a href="{{ Config::get('const.sns_share_url.line') }}"><i class="fab fa-line"></i></a>
+                </div>
             </div>
+            <!-- Grid column -->
         </div>
+        <!-- Grid row-->
     </div>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center">
+        <p>Copyright © 2020 All rights reserved</p>
+    </div>
+    <!-- Copyright -->
     <div class="hr"></div>
 </footer>
+<!-- Footer -->
