46758713cc339b9bf91b83c095459db0e3761050
MasatoshiMoriwaki <m.mori.227@gmail.com>
2020-03-11 16:25:52 +0900

    PCブラウザエラー画面のレイアウト調整

--- src/resources/views/errors/only_mobile.blade.php
+++ src/resources/views/errors/only_mobile.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 <div class="error-wrapper">
-    <div>
-        <div class="error-message">
-            <p></p>
-            <img class="img" src="{{ asset('images/error/question.svg')}}">
-        </div>
-        <div class="microphone">
-            <img class="img" src="{{ asset('images/error/microphone.svg')}}">
-        </div>
-    </div>
+  <div class="error-message-for-other-sp">
+    <p class="error-code">404</p>
+    <p>このページは</p>
+    <p>スマホからしか見れへん･･･</p>
+  </div>
+  <div class="error-img-for-other-sp">
+    <img class="error-img error-img-question" src="{{ asset('images/error/question.svg')}}">
+    <img class="error-img error-img-microphone" src="{{ asset('images/error/microphone.svg')}}">
+  </div>
 </div>
 @endsection
