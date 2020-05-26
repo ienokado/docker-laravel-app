8f5f270268b89d6976aab515b5dc1f3185cff1c4
MasatoshiMoriwaki <m.mori.227@gmail.com>
2020-05-26 22:07:40 +0900

    トップ画面のPC用デザイン実装

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -3,40 +3,43 @@
 @section('content')
 
 <div class="top-page-wrapper">
-  <div class="top-form-group top-animation">
+
+  <div class="top-logo-group {{ \Cookie::get($cookieName) ? "" : "top-animation" }}">
     <img class="logo-img" src="{{ asset('images/top/logo.svg')}}">
     <p class="font-white">あの芸人の出囃子を検索しよう</p>
+  </div>
+  <div class="top-form-group {{ \Cookie::get($cookieName) ? "" : "top-animation" }}">
     <!-- form -->
     <form action="{{ route('debayashi.search') }}" method="post">
       {{ csrf_field() }}
-          <div class="search-box">
-            <input id="search-keyword" type="text" name="search_keyword" class="search-text outline-none" placeholder="コンビ名で検索" required="">
-            <button id="search-button" type="submit" class="search-btn outline-none">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
+      <div class="search-box">
+        <input id="search-keyword" type="text" name="search_keyword" class="search-text outline-none" placeholder="コンビ名で検索" required="">
+        <button id="search-button" type="submit" class="search-btn outline-none">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
     </form>
     <!-- form -->
   </div>
   <div class="comedian-wrapper">
     <div class="comedian">
       <div class="center-pos">
-        <div class="serif top-animation" id="animation-item-last">
+        <div class="serif {{ \Cookie::get($cookieName) ? "" : "top-animation" }}" id="animation-item-last">
           <img class="serif-img" src="{{ asset('images/top/serif.svg')}}">
         </div>
         <div class="male">
           <div class="male-trim">
-            <img class="male-img top-animation" src="{{ asset('images/top/male.svg')}}">
+            <img class="male-img {{ \Cookie::get($cookieName) ? "" : "top-animation" }}" src="{{ asset('images/top/male.svg')}}">
           </div>
         </div>
         <div class="female">
           <div class="female-trim">
-            <img class="female-img top-animation" src="{{ asset('images/top/female.svg')}}">
+            <img class="female-img {{ \Cookie::get($cookieName) ? "" : "top-animation" }}" src="{{ asset('images/top/female.svg')}}">
           </div>
         </div>
         <div class="microphone">
           <div class="microphone-trim">
-            <img class="microphone-img top-animation" src="{{ asset('images/top/microphone.svg')}}">
+            <img class="microphone-img {{ \Cookie::get($cookieName) ? "" : "top-animation" }}" src="{{ asset('images/top/microphone.svg')}}">
           </div>
         </div>
       </div>
@@ -53,14 +56,6 @@
         var cookieValue = "{{ \Cookie::get($cookieName) }}"
 
         if (cookieValue) {
-            // top-animationとクラス名がついているelementを取得
-            var animationItems = document.getElementsByClassName('top-animation');
-
-            var itemArray = Array.prototype.slice.call(animationItems);
-            // top-animationのclassを削除
-            itemArray.forEach(item => {
-                item.classList.remove('top-animation');
-            });
             // フッター表示
             AdjustStyles.displayFooter();
         } else {
